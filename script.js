6c6ec0ec0a8a08510bd8594f9cbe778e2ccadf51
stefan4012it <stefanra555@gmail.com>
2020-07-19 02:40:58 +0200

    change -push

--- script.js
+++ script.js
@@ -7,43 +7,92 @@ const letterPi = document.querySelector('.pi-letter');
 const header = document.querySelector('header');
 
 const container = document.querySelector('.container');
-const text = document.querySelector('#text');
+const text = document.querySelector('.text');
 
 
 const totalTime = 7500;
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
-presentation.addEventListener('click', enterTheVoid);
+let mql = window.matchMedia('(max-device-width: 480px)');
+
+if (mql.matches) {
+    presentation.addEventListener('click', enterTheVoid_Min);
+}else{
+    presentation.addEventListener('click', enterTheVoid);
+}
+
+
+
 function enterTheVoid() {
+    
     leftWing.style.animation = 'moveToLeft 2.8s ease forwards';
     rightWing.style.animation = 'moveToRight 2.8s ease forwards';
     letterU.style.animation = 'moveToLeftLetter 1.8s ease forwards';
     letterT.style.animation = 'moveToLeftLetter 1.8s ease forwards';
     letterPi.style.animation = 'moveToRightLetter 1.8s ease forwards';
-    header.style.visibility = 'visible';
-    header.style.animation = 'intro 2.8s ease forwards';
     
+   
+    presentation.style.animation = 'wenish 2.9s forwards';
+    
+    header.style.visibility = 'visible';
+    header.style.animation = 'intro 2.8s forwards'; 
+    breathAnimation();
+    setInterval(breathAnimation, totalTime);
 }
 
 
-/*breathAnimation();*/
+function enterTheVoid_Min() {
+    leftWing.style.animation = 'moveToLeft 2.8s forwards';
+    rightWing.style.animation = 'moveToRight 2.8s forwards';
+    letterU.style.animation = 'moveToLeftLetter_Min 1.8s forwards';
+    letterT.style.animation = 'moveToLeftLetter_Min 1.8s forwards';
+    letterPi.style.animation = 'moveToRightLetter_Min 1.8s forwards';
+    presentation.style.animation = 'wenish 2.9s forwards';
+    
+    header.style.visibility = 'visible';
+    header.style.animation = 'intro 2.8s forwards'; 
+    setTimeout(() => {
+        presentation.remove();
+        leftWing.remove();
+        rightWing.remove();
+        letterU.remove();
+        letterPi.remove();
+        letterT.remove();
+        breathAnimation();
+        setInterval(breathAnimation, totalTime);
+                }, 2800);
+        
+}
+
+
+
 
 function breathAnimation () {
-    text.innerHTML = 'New Era!';
-    container.className = 'container grow';
     
     setTimeout(() => {
-        text.innerText = 'Comming soon...'
         
-        setTimeout( () => {
-            text.innerText = 'Unija projekt'
-            container.className = 'container shrink'
-        }, holdTime)
-    }, breathTime)
+        text.innerHTML = 'New era!';
+
+        container.className = 'container grow';
+
+        setTimeout(() => {
+
+            text.innerText = 'Coming soon...';
+
+            setTimeout( () => {
+
+                text.innerText = 'Unija projekt';
+
+                container.className = 'container shrink';
+                
+            }, holdTime)
+        }, breathTime)
+    }, 3500)
+    
 }
 
-setInterval(breathAnimation, totalTime);
+
 
 
 
